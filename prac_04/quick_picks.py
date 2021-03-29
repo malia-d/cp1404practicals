23826f1e0f49a3985604b938d194c35d10050970
maliadmello <malia.dmello@my.jcu.edu.au>
2021-03-30 00:30:23 +1000

    Add quick picks program.

---
+++ prac_04/quick_picks.py
@@ -0,0 +1,24 @@
+import random
+
+NUMBERS_PER_LINE = 6
+MAXIMUM_NUMBER = 45
+MINIMUM_NUMBER = 1
+
+
+def main():
+    number_of_quick_picks = int(input("How many quick picks? "))
+    for i in range(number_of_quick_picks):
+        quick_picks = []
+        # generate a random number between the minimum and maximum values
+        for j in range(NUMBERS_PER_LINE):
+            number = random.randint(MINIMUM_NUMBER, MAXIMUM_NUMBER)
+            # replaces any numbers that have already appeared in the list
+            while number in quick_picks:
+                number = random.randint(MINIMUM_NUMBER, MAXIMUM_NUMBER)
+            quick_picks.append(number)
+        quick_picks.sort()  # sorts the numbers in ascending order
+        # print the list with a space separating each number, six numbers per line, and the user given number of lines
+        print(" ".join("{:2}".format(number) for number in quick_picks))
+
+
+main()
