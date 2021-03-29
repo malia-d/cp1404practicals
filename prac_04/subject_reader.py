f0c45a6aab1d2277d46ae6eabf89cb5c06362e41
maliadmello <malia.dmello@my.jcu.edu.au>
2021-03-29 23:49:15 +1000

    Add function docstrings.

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -7,6 +7,7 @@ FILENAME = "subject_data.txt"
 
 
 def main():
+    """Obtain the subject data as a list and display it one list per line."""
     data = get_data()
     display_subject_details(data)
 
@@ -16,13 +17,9 @@ def get_data():
     data = []
     input_file = open(FILENAME)
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
         data.append(parts)
         print("----------")
     input_file.close()
@@ -30,6 +27,7 @@ def get_data():
 
 
 def display_subject_details(data):
+    """Print the subject, lecturer and number of students enrolled in the subject."""
     for subject_details in data:
         print("{} is taught by {:1} and has {:2} students".format(*subject_details))
 
