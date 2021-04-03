1b83d203cec9bbf393be4d43af68641e270710cd
maliadmello <malia.dmello@my.jcu.edu.au>
2021-04-03 21:33:12 +1000

    Add module docstring.

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -1,5 +1,14 @@
-NAME_TO_CODE = {"AliceBlue": "f0f8ff", "pink": "ffc0cb", "LightSlateBlue": "8470ff", "gold1": "ffd700", "firebrick2":
-    "ee2c2c", "MediumPurple1": "ab82ff", "PaleGreen2": "90ee90", "tan1": "ffa54f", "gray1": "030303", "white": "ffffff"}
+"""
+Receive a user input, colour_name, and if it is contained within the dictionary, NAME_TO_CODE, it prints the colour name
+and code, and continues to prompt the user for a new input. If it is not contained in the dictionary, it prints an
+invalid input message and prompts the user for a new input.
+
+Hex Colours. Created by Malia, April 2021.
+"""
+
+NAME_TO_CODE = {"AliceBlue": "f0f8ff", "pink": "ffc0cb", "LightSlateBlue": "8470ff", "gold1": "ffd700",
+                "firebrick2": "ee2c2c", "MediumPurple1": "ab82ff", "PaleGreen2": "90ee90", "tan1": "ffa54f",
+                "gray1": "030303", "white": "ffffff"}
 
 colour_name = input("Enter colour name: ")
 # While the user input is not blank the loop proceeds, but if the user input is blank the loop stops.
