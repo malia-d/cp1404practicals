267cbd6abe9a05f78890944dbb74dc9f6f6c4470
maliadmello <malia.dmello@my.jcu.edu.au>
2021-04-03 23:09:02 +1000

    Add module docstring.

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,3 +1,12 @@
+"""
+This program asks the user to input a string of text which is iterated through, adding each word to a dictionary as a
+key and the number of occurrences of each word as a value. It then prints the each word and it's number of occurrences.
+The length of the longest word is determined so that, when printed, the numbers are aligned in one column using string
+formatting.
+
+Word Occurrences. Created by Malia, April 2021.
+"""
+
 word_to_count = {}
 text = input("Text: ")
 words = text.split()
@@ -9,7 +18,7 @@ for word in words:
         word_to_count[word] = 1
 word_list = list(word_to_count.keys())  # Creating a list of the dictionary
 word_list.sort()  # Sorting the list into alphabetical order
-longest_word = max(len(word) for word in word_list)
+longest_word = max(len(word) for word in word_list) # Determining the number of characters in the longest word
 for word in word_list:
     # Using string formatting and the number of characters in the longest word to align the values.
     print("{:{}} : {}".format(word, longest_word, word_to_count[word]))
