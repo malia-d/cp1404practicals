d1bb65ca7420d4b40dfaab6c27aac00cdd4ab28d
maliadmello <malia.dmello@my.jcu.edu.au>
2021-04-19 10:23:27 +1000

    Test get_age() and is_vintage() methods.

---
+++ prac_06/guitar_test.py
@@ -0,0 +1,20 @@
+"""
+
+
+Guitar Test. Created by Malia, April 2021.
+"""
+
+from prac_06.guitar import Guitar
+
+
+def main():
+    guitar = Guitar("Gibson L-5 CES", 1922, 16035.40)
+    another_guitar = Guitar("Another Guitar", 2013, 1512.9)
+
+    print("{} get_age() - Expected 99. Got {}".format(guitar.name, guitar.get_age()))
+    print("{} get_age() - Expected 8. Got {}".format(another_guitar.name, another_guitar.get_age()))
+    print("{} is_vintage() - Expected True. Got {}".format(guitar.name, guitar.is_vintage()))
+    print("{} is_vintage() - Expected False. Got {}".format(another_guitar.name, another_guitar.is_vintage()))
+
+
+main()
