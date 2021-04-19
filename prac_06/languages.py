d8519b62f5c57c27a5f6e959f6528ef2a52c179a
maliadmello <malia.dmello@my.jcu.edu.au>
2021-04-19 10:12:18 +1000

    Error check __str__ function.

---
+++ prac_06/languages.py
@@ -0,0 +1,18 @@
+"""
+
+
+Languages. Created by Malia, April 2021.
+"""
+
+from prac_06.programming_language import ProgrammingLanguage
+
+
+def main():
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+
+    print(python)
+
+
+main()
