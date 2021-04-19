b08555f84b80396449e46f930cb0495c8a113032
maliadmello <malia.dmello@my.jcu.edu.au>
2021-04-19 10:26:42 +1000

    Get data for testing.

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -19,5 +19,9 @@ def main():
         print("{} ({}) : ${:.2f} added.".format(name, year, cost))
         name = input("Name: ")
 
+    guitars.append(Guitar("Fender Stratocaster", 2014, 765.40))
+    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+
 
 main()
