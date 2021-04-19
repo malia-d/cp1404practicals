29583afbe4d79ffb93a255db779e1d9c1458c2ff
maliadmello <malia.dmello@my.jcu.edu.au>
2021-04-19 10:27:51 +1000

    Print guitar details.

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -23,5 +23,14 @@ def main():
     guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
     guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
 
+    print("These are my guitars:")
+    for i, guitar in enumerate(guitars):
+        if guitar.is_vintage():
+            vintage_string = " (vintage)"
+            print("Guitar {}: {} ({}), worth ${:.2f}{}".format(i + 1, guitar.name, guitar.year, guitar.cost,
+                                                               vintage_string))
+        else:
+            print("Guitar {}: {} ({}), worth ${:.2f}".format(i + 1, guitar.name, guitar.year, guitar.cost))
+
 
 main()
