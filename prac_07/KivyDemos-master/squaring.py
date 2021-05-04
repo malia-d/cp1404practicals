5ce676e99d9c4212e67e16bc8b468fdf9c4e3eeb
maliadmello <malia.dmello@my.jcu.edu.au>
2021-05-04 18:49:13 +1000

    Modify Squaring program.

--- prac_07/KivyDemos-master/squaring.py
+++ prac_07/KivyDemos-master/squaring.py
@@ -1,10 +1,3 @@
-"""
-CP1404/CP5632 Practical
-Kivy GUI program to square a number
-Lindsay Ward, IT@JCU
-Started 13/10/2015
-"""
-
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.core.window import Window
@@ -13,18 +6,18 @@ __author__ = 'Lindsay Ward'
 
 
 class SquareNumberApp(App):
-    """ SquareNumberApp is a Kivy App for squaring a number """
     def build(self):
-        """ build the Kivy app from the kv file """
-        Window.size = (200, 100)
+        Window.size = (400, 100)
         self.title = "Square Number"
         self.root = Builder.load_file('squaring.kv')
         return self.root
 
     def handle_calculate(self, value):
-        """ handle calculation (could be button press or other call), output result to label widget """
-        result = value ** 2
-        self.root.ids.output_label.text = str(result)
+        try:
+            result = int(value) ** 2
+            self.root.ids.output_label.text = str(result)
+        except ValueError:
+            self.root.ids.output_label.text = "Invalid Input"
 
 
 SquareNumberApp().run()
