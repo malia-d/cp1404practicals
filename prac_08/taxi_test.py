363988ddadf06572ab7780eb6cdc076b78702d2a
maliadmello <malia.dmello@my.jcu.edu.au>
2021-05-09 22:26:46 +1000

    Add module and function docstrings for clarity.

--- prac_08/taxi_test.py
+++ prac_08/taxi_test.py
@@ -1,7 +1,14 @@
+"""
+Test the Taxi class by creating a taxi and modifying the distance driven.
+
+Taxi Test. Created by Malia D'Mello, May 2021.
+"""
+
 from prac_08.taxi import Taxi
 
 
 def main():
+    """Test Taxi class"""
     taxi = Taxi("Prius 1", 100, 1.23)
     taxi.drive(40)
     print(taxi)
