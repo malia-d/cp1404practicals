9453d7c11ea3962ae10aa71ab1d940c98b38073c
maliadmello <malia.dmello@my.jcu.edu.au>
2021-05-09 22:23:08 +1000

    Write taxi test program.

---
+++ prac_08/taxi_test.py
@@ -0,0 +1,13 @@
+from prac_08.taxi import Taxi
+
+
+def main():
+    taxi = Taxi("Prius 1", 100, 1.23)
+    taxi.drive(40)
+    print(taxi)
+    taxi.start_fare()
+    taxi.drive(100)
+    print(taxi)
+
+
+main()
